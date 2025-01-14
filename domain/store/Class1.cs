4ed59bac0c8bfefd36335c1fb9fbfb617db21d03
Pilopan <n.dashko59@gmail.com>
2025-01-14 22:56:47 +0300

    Описать сущность Book

--- domain/store/Class1.cs
+++ domain/store/Class1.cs
@@ -1,7 +1,13 @@
 ﻿namespace store
 {
-    public class Class1
+    public class Book
     {
-
+        public int Id { get; }
+        public string Title { get; }
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
     }
 }
