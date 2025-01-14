7c2cca25079b38a32f5f09f91ad92a085527c82a
Pilopan <n.dashko59@gmail.com>
2025-01-14 23:08:02 +0300

    Добавить интерфейс репозитория книг

---
+++ domain/store/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store
+{
+    public interface IBookRepository
+    {
+        //Добавляел All, потому что возвращает несколько объектов, а не один
+        //Интерфейсом определили метод для получения книг по названию (части названия)
+        Book[] GetAllByTitle(string titlePart);
+    }
+}
