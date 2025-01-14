0497470018bf056f22ace699af3001e24e8d9d60
Pilopan <n.dashko59@gmail.com>
2025-01-14 23:39:56 +0300

    Создать уровень доступа к данным Store.Memory

--- infrastructure/Store.Memory/Class1.cs
+++ infrastructure/Store.Memory/Class1.cs
@@ -1,8 +1,20 @@
-﻿
+﻿using store;
+using System.Linq;
+
 namespace Store.Memory
 {
     public class BookRepository : IBookRepository
     {
-
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Книга номер 1"),
+            new Book(1, "Книга номер 2"),
+            new Book(1, "Книга номер 3"),    
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
     } 
 }
