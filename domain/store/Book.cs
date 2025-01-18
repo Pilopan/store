1175947d14612726a8361721ff1b25844a3d03b7
Pilopan <n.dashko59@gmail.com>
2025-01-18 16:00:43 +0300

    Реализовать поиск по ISBN и Author

---
+++ domain/store/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Isbn { get; }
+        public string Author { get; }
+        public string Title { get; }
+        public Book(int id, string isbn, string author, string title)
+        {
+            Id = id;
+            Title = title;
+            Isbn = isbn;
+            Author = author;
+        }
+    }
+}
